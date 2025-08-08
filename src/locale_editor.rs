6e8740aa2914610ade1a75ffb83b543f3eead04d
lavafrai <lavafrai@yandex.ru>
2025-08-09 00:16:28 +0300

    feat: implement asynchronous loading for locale state updates in UI

--- src/locale_editor.rs
+++ src/locale_editor.rs
@@ -6,15 +6,17 @@ use crate::game_controller::{
 use arboard::Clipboard;
 use std::path::Path;
 use std::rc::Rc;
-use slint::{ModelRc, SharedString, VecModel};
+use std::sync::Arc;
+use std::thread;
+use slint::{ModelRc, SharedString, VecModel, invoke_from_event_loop};
 use crate::locale_cotroller::LocaleController;
 
 slint::include_modules!();
 
 pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
     // Initialize game controller
-    let controller = Rc::new(create_game_controller(game_folder));
-    let locale_controller = Rc::new(LocaleController::new(&controller));
+    let controller = Arc::new(create_game_controller(game_folder));
+    let locale_controller = Arc::new(LocaleController::new(&controller));
     let initial_debug_enabled = is_debug_localization_enabled(&controller);
     let initial_localization_files_created = is_localization_files_created(&controller);
     // Initialize UI
@@ -22,7 +24,7 @@ pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
     let ui_weak = ui.as_weak();
 
     if initial_debug_enabled && initial_localization_files_created {
-        update_locale_state(&ui, &locale_controller, None);
+        update_locale_state(&ui, Arc::clone(&locale_controller), None);
     }
 
 
@@ -36,7 +38,7 @@ pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
 
     // Open config.blk
     {
-        let controller = Rc::clone(&controller);
+        let controller = Arc::clone(&controller);
         ui.on_request_open_config_blk(move || {
             let config_path = get_config_blk_path(&controller);
             opener::open(config_path).expect("Failed to open config file");
@@ -45,8 +47,8 @@ pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
 
     // Recheck debug localization state
     {
-        let controller = Rc::clone(&controller);
-        let locale_controller = Rc::clone(&locale_controller);
+        let controller = Arc::clone(&controller);
+        let locale_controller = Arc::clone(&locale_controller);
         let ui_handle = ui_weak.clone();
         ui.on_request_localization_state_recheck(move || {
             let enabled = is_debug_localization_enabled(&controller);
@@ -60,16 +62,16 @@ pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
             }
 
             if enabled && files_created {
-                update_locale_state(&ui_handle.unwrap(), &locale_controller, None);
+                update_locale_state(&ui_handle.unwrap(), Arc::clone(&locale_controller), None);
             }
         });
     }
 
     {
-        let locale_controller = Rc::clone(&locale_controller);
+        let locale_controller = Arc::clone(&locale_controller);
         let ui_handle = ui_weak.clone();
         ui.on_locale_set(move |new_locale| {
-            update_locale_state(&ui_handle.unwrap(), &locale_controller, Some(new_locale.to_string()));
+            update_locale_state(&ui_handle.unwrap(), Arc::clone(&locale_controller), Some(new_locale.to_string()));
         });
     }
 
@@ -82,25 +84,33 @@ pub fn run(game_folder: &Path) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn update_locale_state(ui: &LocaleEditorUI, locale_controller: &Rc<LocaleController>, selected_locale: Option<String>) {
-    let available_locales = locale_controller.get_available_locales();
-    let shared_locales: Vec<SharedString> = available_locales
-        .iter()
-        .map(|locale| SharedString::from(locale.clone()))
-        .collect();
-    let model = Rc::new(VecModel::from(shared_locales));
-    let model_rc = ModelRc::from(model.clone());
-    ui.set_available_locales(model_rc);
-
-    let selected_locale = selected_locale
-        .or_else(|| available_locales.first().cloned())
-        .unwrap_or_default();
-    ui.set_selected_locale(SharedString::from(selected_locale.clone()));
-
-    let locale_texts = locale_controller.get_locale_texts(&selected_locale);
-    let locale_texts_with_restriction_count = locale_texts.iter()
-        .filter(|text| text.max_chars > 0)
-        .count();
-    println!("Locale '{}' has {} texts with restrictions", selected_locale, locale_texts_with_restriction_count);
-    // println!("{:?}", locale_texts);
+fn update_locale_state(ui: &LocaleEditorUI, locale_controller: Arc<LocaleController>, selected_locale: Option<String>) {
+    // Start asynchronous loading
+    ui.set_loading_locales(true);
+    let ui_weak = ui.as_weak();
+    let controller = Arc::clone(&locale_controller);
+    let selected_opt = selected_locale.clone();
+    thread::spawn(move || {
+        // Load data in background
+        let available_locales = controller.get_available_locales();
+        let selected = selected_opt
+            .or_else(|| available_locales.first().cloned())
+            .unwrap_or_default();
+        let texts = controller.get_locale_texts(&selected);
+        println!("Locale '{}' has {} texts", selected, texts.len());
+        // Update UI in UI thread
+        invoke_from_event_loop(move || {
+            if let Some(handle) = ui_weak.upgrade() {
+                let shared: Vec<SharedString> = available_locales
+                    .iter()
+                    .map(|l| SharedString::from(l.clone()))
+                    .collect();
+                let model = Rc::new(VecModel::from(shared));
+                let model_rc = ModelRc::from(model.clone());
+                handle.set_available_locales(model_rc);
+                handle.set_selected_locale(SharedString::from(selected.clone()));
+                handle.set_loading_locales(false);
+            }
+        }).expect("");
+    });
 }
